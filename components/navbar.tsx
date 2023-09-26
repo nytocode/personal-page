885405598cc1008e4f94e4397801ac8ebf322bc5
nytocode <anto4te.dev@gmail.com>
2023-09-26 18:40:25 +0200

    Getting Started

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+import ThemeToggle from "./theme-toggle";
+
+const Navbar = () => {
+  return (
+    <header className="w-[1000px] mx-auto">
+      <ThemeToggle />
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ components/theme-toggle.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
+import { useCallback } from "react";
+
+const ThemeToggle = () => {
+  const { setTheme, theme } = useTheme();
+
+  const onToggleTheme = useCallback(() => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  }, [theme]);
+
+  return (
+    <Button
+      className={cn(
+        "bg-foreground hover:bg-foreground/80 transition-all dark:bg-primary dark:hover:bg-primary/80",
+      )}
+      onClick={onToggleTheme}
+    >
+      <SunIcon
+        className={cn(
+          "h-[1.2rem] w-[1.2rem] scale-0 transition-all dark:scale-100",
+        )}
+      />
+      <MoonIcon
+        className={cn(
+          "h-[1.2rem] w-[1.2rem] absolute scale-100 transition-all  dark:scale-0",
+        )}
+      />
+    </Button>
+  );
+};
+
+export default ThemeToggle;
