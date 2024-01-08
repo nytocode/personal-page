2f5f973d4dffafec76f6c37c0401d41bef280a12
nytocode <anto4te.dev@gmail.com>
2024-01-08 10:18:48 +0100

    add posthog for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { IBM_Plex_Mono } from "next/font/google";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-import GoogleAnalytics from "@/components/google-analytics";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Suspense } from "react";
+import { PHProvider, PostHogPageview } from "@/providers/posthog-provider";
 
 const ibm_plex_mono = IBM_Plex_Mono({
   weight: ["100", "300", "400", "500", "700"],
@@ -24,21 +25,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Suspense>
+        <PostHogPageview />
+      </Suspense>
       <body className={ibm_plex_mono.className}>
-        <GoogleAnalytics />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
-          <div className="h-screen w-screen flex flex-col">
-            <Navbar />
-            <div className="flex-1 overflow-y-scroll  px-5 py-[50px]">
-              <div className="w-[90%] max-w-[700px] mx-auto ">{children}</div>
+          <PHProvider>
+            <div className="h-screen w-screen flex flex-col">
+              <Navbar />
+              <div className="flex-1 overflow-y-scroll  px-5 py-[50px]">
+                <div className="w-[90%] max-w-[700px] mx-auto ">{children}</div>
+              </div>
+              <Footer />
             </div>
-            <Footer />
-          </div>
+          </PHProvider>
         </ThemeProvider>
         <Toaster richColors position="bottom-center" />
       </body>
