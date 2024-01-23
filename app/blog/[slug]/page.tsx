fc0558805a4531139ff0f3fb575506590bfd160d
nytocode <anto4te.dev@gmail.com>
2024-01-23 18:23:44 +0100

    add first post: node-auth

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { getPostBySlug } from "@/lib/mdx";
 
-export default async function Post() {
-  const { meta, content } = await getPostBySlug("post-prova");
+export default async function Post({ params }: { params: { slug: string } }) {
+  const { meta, content } = await getPostBySlug(params.slug);
 
   return <div className="prose dark:prose-invert">{content}</div>;
 }
